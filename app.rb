559822948372e81a4b19b1f2153d86ed5e178b13
Chris Continanza <christopher.continanza@gmail.com>
2012-01-07 01:01:08 -0800

    hide trigger

--- app.rb
+++ app.rb
@@ -42,8 +42,8 @@ class App < Sinatra::Base
   post '/request/:id' do
     DB[:requests].insert(:resource_id => params[:id], :params => params.to_json)
     
-    if params['REQUEST_METHOD'] == 'GET' && params['QUERY_STRING'].include?('post=')
-      {:REQUEST_METHOD => 'POST', :body => params['QUERY_STRING']}.to_json
+    if params['REQUEST_METHOD'] == 'GET' && params['QUERY_STRING'].include?('post=true')
+      {:REQUEST_METHOD => 'POST', :body => params['QUERY_STRING'].gsub('post=true','')}.to_json
     else
       {}.to_json
     end
